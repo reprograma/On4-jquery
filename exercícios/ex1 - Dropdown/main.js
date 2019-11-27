d831d8597867c4d9dc72b894cda79d8a2b3c939b
caguimaraes <c.guimaraes88@yahoo.com>
2019-11-27 17:43:29 -0300

    Exercises

--- exercícios/ex1 - Dropdown/main.js
+++ exercícios/ex1 - Dropdown/main.js
@@ -1,22 +1,15 @@
 $(document).ready(function() {
-  $("#menu li a").mouseover(function() {
-    var index = $("#menu li a").index(this);
+  $("#menu li a").click(function(e) {
+    e.preventDefault()
+    let index = $("#menu li a").index(this);
     $("#menu li")
       .eq(index)
       .children("ul")
       .slideDown();
-
-    if (
-      $(this)
-        .siblings("ul")
-        .size() > 0
-    ) {
-      return false;
-    }
   });
 
   $("#menu li").mouseleave(function() {
-    var index = $("#menu li").index(this);
+    let index = $("#menu li").index(this);
     $("#menu li")
       .eq(index)
       .children("ul")
